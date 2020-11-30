dee1392c57fe7561b257a5e35cb8adddc925dbae
Andrey <a.g.terentyev@gmail.com>
2020-11-30 11:27:36 +0300

    highlight buttons on hover

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Styles/Button.css";
 
 export default function Button(props) {
+  const [highlightStyle, setHighlightStyle] = useState({
+    top: 0,
+    left: 0,
+    opacity: 0,
+  });
+
+  function moveHighlight(e) {
+    setHighlightStyle({
+      left: e.nativeEvent.layerX - 100,
+      top: e.nativeEvent.layerY - 100
+    });
+  }
+
+  function hideHighlight(e) {
+    setHighlightStyle({
+      opacity: 0
+    });
+  }
+
   const { value, handleClick } = props;
   const spansTwo = value === "0" ? "spans-two" : "";
   return (
-    <button
+    <div
       className={`button ${spansTwo}`}
       onClick={(e) => handleClick(e, value)}
+      onMouseMove={moveHighlight}
+      onMouseOut={hideHighlight}
     >
-      {value}
-    </button>
+      <div class="value">{value}</div>
+      <div className="highlight" style={highlightStyle}></div>
+    </div>
   );
 }
